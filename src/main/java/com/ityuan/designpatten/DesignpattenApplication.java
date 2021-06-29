7b2d285c78a699c163dcce8525fae5ca943d38dc
yuanchaoxin <lboy_2015@163.com>
2021-06-29 22:05:06 +0800

    init

---
+++ src/main/java/com/ityuan/designpatten/DesignpattenApplication.java
@@ -0,0 +1,13 @@
+package com.ityuan.designpatten;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignpattenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DesignpattenApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ityuan/designpatten/DesignpattenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ityuan.designpatten;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignpattenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
